aaf74459fdd50e5b8545df25231f9e3482e9653a
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2024-10-01 15:14:38 +0300

    Add files via upload

---
+++ models/saraha.js
@@ -0,0 +1,14 @@
+// models/order.js
+const mongoose = require('mongoose');
+
+const sarahaSchema = new mongoose.Schema({
+    uid: { type: mongoose.Schema.Types.Mixed, required: true },
+    username: { type: String, required: true },
+    date: { type: String, required: true },
+    numberToOrderBy: { type: Number, required: true }
+});
+  
+const Saraha = mongoose.model('Saraha', sarahaSchema);
+  
+module.exports = Saraha;
+  
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+// Middleware
+// app.use(cors());
+
+// Use the CORS middleware
+app.use(cors({
+  origin: '*' // or use '*' to allow all origins
+}));
+
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://omarvenom22:JPZ4jK7U8zECbbxB@cluster0.p3oak.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Routes
+
+
+const sarahaRoutes = require('./routes/Saraha');
+app.use('/saraha', sarahaRoutes);
+
+
+
+
+
+
+// Create HTTP Server
+const server = http.createServer(app);
+
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
